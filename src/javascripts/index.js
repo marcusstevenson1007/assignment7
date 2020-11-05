6420d649eaf4451e96fb3bb30b1b72e907919806
Marcus Stevenson <marcusstevenson1007@gmail.com>
2020-11-04 17:58:50 -0700

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,38 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function render(){
+    let c =document.querySelector('canvas')
+    if(c.getContext){
+        let ctx = c.getContext('2d')
+        //black square
+        ctx.clearRect(0, 0, 1000, 600)
+        ctx.fillStyle = document.getElementById('c').value
+
+        //red circle
+        ctx.fillStyle = "red"
+        ctx.beginPath()
+        ctx.arc(document.getElementById('x').value, 150, 75, 0, 2 * Math.PI)
+        ctx.fill()
+
+          //blue circle
+          ctx.fillStyle = "blue"
+          ctx.beginPath()
+          ctx.arc(document.getElementById('y').value, 300, 75, 0, 2 * Math.PI)
+          ctx.fill()
+
+           //orange circle
+           ctx.fillStyle = "orange"
+           ctx.beginPath()
+           ctx.arc(document.getElementById('z').value, 450, 75, 0, 2 * Math.PI)
+           ctx.fill()
+    }
+}
+
+document.body.onload = render;
+document.getElementById('x').onchange = render;
+document.getElementById('y').onchange = render;
+document.getElementById('z').onchange = render;
